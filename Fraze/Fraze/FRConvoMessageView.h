44c64ebe2a0cbf01b93a6049b0633ebf5b6645da
Si Te Feng <fengsite@hotmail.com>
2015-01-19 23:13:11 -0800

    Started working on Convo View controller

---
+++ Fraze/Fraze/FRConvoMessageView.h
@@ -0,0 +1,13 @@
+//
+//  FRConvoMessageView.h
+//  Fraze
+//
+//  Created by Si Te Feng on 1/19/15.
+//  Copyright (c) 2015 Si Te Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRConvoMessageView : UIView
+
+@end
---
+++ Fraze/Fraze/FRConvoMessageView.m
@@ -0,0 +1,21 @@
+//
+//  FRConvoMessageView.m
+//  Fraze
+//
+//  Created by Si Te Feng on 1/19/15.
+//  Copyright (c) 2015 Si Te Feng. All rights reserved.
+//
+
+#import "FRConvoMessageView.h"
+
+@implementation FRConvoMessageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
